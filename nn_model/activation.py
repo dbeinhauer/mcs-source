ab85da7dad7795d10701149e96881ce09cb4bcdb
Richard Kraus <krausri1@fit.cvut.cz>
2025-01-30 00:39:58 +0100

    ensure leakytanh approaches 0 for x -> -inf

--- nn_model/activation.py
+++ nn_model/activation.py
@@ -3,10 +3,14 @@ import torch.nn as nn
 
 
 class LeakyTanh(nn.Module):
-    def __init(self, scale: float = 5., leak: float = .001):
+    def __init__(self, scale: float = 2.5, leak: float = .001, steepness: float = .2, beta: float = 20):
         super().__init__()
         self.scale = scale
         self.leak = leak
+        self.steepness = steepness
+        self.beta = beta
 
     def forward(self, x):
-        return self.scale * torch.tanh(x / self.scale) + self.leak * x
+        return (self.scale * torch.tanh(self.steepness * x)  # tanh
+                + self.scale  # move up so that lim -inf is 0
+                + self.leak * torch.nn.functional.softplus(x, beta=self.beta))  # introduce leak for x -> inf
