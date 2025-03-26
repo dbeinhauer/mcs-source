0149c906505e5c12c04819e4b1945a5362fad007
Richard Kraus <richard.kraus138@gmail.com>
2025-03-26 23:24:07 +0100

    add neuron X, Y position and orientation preference

--- nn_model/globals.py
+++ nn_model/globals.py
@@ -3,8 +3,10 @@ This source code contains definition of all global variables that
 are used across multiple source files. Typically information
 about the layer and model parameters.
 """
-
+import pickle
 from pathlib import Path
+
+import numpy as np
 
 from nn_model.type_variants import (
     LayerType,
@@ -101,10 +103,10 @@ SEPARATE_MODELS = [
 DEFAULT_PATHS = {
     PathDefaultFields.TRAIN_DIR.value: f"{PROJECT_ROOT}/dataset/train_dataset/compressed_spikes/trimmed/size_{TIME_STEP}",
     PathDefaultFields.TEST_DIR.value: f"{PROJECT_ROOT}/dataset/test_dataset/compressed_spikes/trimmed/size_{TIME_STEP}",
-    PathDefaultFields.SUBSET_DIR.value: f"{PROJECT_ROOT}/dataset/model_subsets/size_{int(SIZE_MULTIPLIER*100)}.pkl",
+    PathDefaultFields.SUBSET_DIR.value: f"{PROJECT_ROOT}/dataset/model_subsets/size_{int(SIZE_MULTIPLIER * 100)}.pkl",
     PathDefaultFields.MODEL_DIR.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_results/best_models/",
     PathDefaultFields.EXPERIMENT_SELECTION_PATH.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_subsets/experiments/experiments_subset_10.pkl",
-    PathDefaultFields.NEURON_SELECTION_PATH.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_subsets/neurons/model_size_{int(SIZE_MULTIPLIER*100)}_subset_10.pkl",
+    PathDefaultFields.NEURON_SELECTION_PATH.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_subsets/neurons/model_size_{int(SIZE_MULTIPLIER * 100)}_subset_10.pkl",
     PathDefaultFields.SELECTION_RESULTS_DIR.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_results/neuron_responses/",
     PathDefaultFields.FULL_EVALUATION_DIR.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_results/full_evaluation_results/",
     PathDefaultFields.NEURON_MODEL_RESPONSES_DIR.value: f"{PROJECT_ROOT}/evaluation_tools/evaluation_results/neuron_model_responses/",
@@ -119,10 +121,20 @@ if TIME_STEP == 1:
         f"{PROJECT_ROOT}/dataset/test_dataset/trimmed_spikes"
     )
 
-
 # All default paths of the plots.
 DEFAULT_PLOT_PATHS = {
     PathPlotDefaults.NEURON_MODULE_SEPARATE.value: f"{PROJECT_ROOT}/evaluation_tools/plot_images/dnn_module_dependencies_separate.png",
     PathPlotDefaults.NEURON_MODULE_TOGETHER.value: f"{PROJECT_ROOT}/evaluation_tools/plot_images/dnn_module_dependencies_together.png",
-    PathPlotDefaults.MEAN_LAYER_RESPONSES.value: f"{PROJECT_ROOT}/evaluation_tools/plot_images/mean_layer_responses_size_{int(SIZE_MULTIPLIER*100)}.png",
+    PathPlotDefaults.MEAN_LAYER_RESPONSES.value: f"{PROJECT_ROOT}/evaluation_tools/plot_images/mean_layer_responses_size_{int(SIZE_MULTIPLIER * 100)}.png",
 }
+
+with open(f"{PROJECT_ROOT}/testing_dataset/pos_and_ori_dictionary.pickle", 'rb') as f:
+    POS_ORI_DICT = pickle.load(f)
+
+with open(DEFAULT_PATHS[PathDefaultFields.SUBSET_DIR.value], 'rb') as f:
+    NEURON_SELECTION = pickle.load(f)
+
+for layer, xyo in POS_ORI_DICT.items():
+    for attr in ('x', 'y', 'ori'):
+        subset_filter = NEURON_SELECTION[layer].astype(int)
+        POS_ORI_DICT[layer][attr] = np.array(POS_ORI_DICT[layer][attr])[subset_filter].astype(float)
